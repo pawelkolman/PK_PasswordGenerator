750b799c7772c53dc1f9051451953f57183ad458
PK_ThinkPad <pawelkolman12@gmail.com>
2020-09-30 17:44:39 +0200

    PK_PasswordGenerator 1.0

---
+++ PK_Generator/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+import random
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'PK_Generator/home.html')
+
+def password(request):
+    
+    characters = []
+    
+    length = int(request.GET.get('length', 10)) # '10' is a default parameter
+    
+    if request.GET.get('lowercase'):
+        characters.extend(list('abcdefghijklmnopqrstuvwxyz'))
+        
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+        
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+        
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*()'))
+        
+    if request.GET.get('excludesimilar'):
+        similar = list('iIl|0Oo')
+        for char in similar:
+            if char in characters:
+                characters.remove(char)
+    
+    password = ''
+    for i in range(length):
+        password += random.choice(characters)
+    
+    return render(request, 'PK_Generator/password.html', {'password':password})
---
+++ README.md
@@ -0,0 +1,9 @@
+[PK_PasswordGenerator](http://PKPasswordGenerator.pythonanywhere.com/)
+================================================================================
+Basic password generator bulit with Django
+
+Includes
+--------------------------------------------------------------------------------
+- responsive design
+- dark theme
+- copy password button
